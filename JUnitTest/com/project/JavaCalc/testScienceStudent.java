72ddcfefcb8ffbad18a8406d46ad95db25bae000
1amBulletproof <brandon.tarney@gmail.com>
2015-11-26 20:54:24 -0500

    Rework for JUnit @after & @before & Hamcrest & mockito (or other mock framework)

--- JUnitTest/com/project/JavaCalc/testScienceStudent.java
+++ JUnitTest/com/project/JavaCalc/testScienceStudent.java
@@ -1,9 +1,9 @@
 
 package com.project.JavaCalc;
 
-import static org.junit.Assert.*;
+import org.junit.*;
+import org.hamcrest.*;
 
-import org.junit.Test;
 
 
 
@@ -11,7 +11,9 @@ import org.junit.Test;
 
 public class testScienceStudent {
 
-	private double calcScienceFinal(int midtermExam, int finalExam, int researchPaper){
+	ScienceStudent sciStudent;
+	
+	private double mockCalcScienceFinal(int midtermExam, int finalExam, int researchPaper){
 		double midtermWeight = 0.4;
 		double finalExamWeight = 0.4;
 		double researchWeight = 0.2;
@@ -19,10 +21,13 @@ public class testScienceStudent {
 				 + researchPaper * researchWeight);
 	}
 	
+	@Before
+	public void initialize() {
+		sciStudent = new ScienceStudent();
+	}
 	
 	@Test
-	public void testMidterm() {
-		ScienceStudent sciStudent = new ScienceStudent();
+	public void normalTestMidterm() {
 		int[] midterm = {0,10,20,30,40,50,60,70,80,90,100};
 		int finalExam = 100;
 		int researchPaper = 100;
@@ -30,17 +35,17 @@ public class testScienceStudent {
 		for (int i = 0; i < midterm.length; i++){
 			sciStudent.calculate(midterm[i], finalExam, researchPaper);
 			System.out.println(sciStudent.getFinalNumericGrade());
-			double finalGrade = calcScienceFinal(midterm[i], finalExam, researchPaper);
-			assertEquals(finalGrade, sciStudent.getFinalNumericGrade(), 0.1);
-			System.out.println(finalGrade);
-
-			
-			
-			
-		}
-		
-		
-		
+			double finalGrade = mockCalcScienceFinal(midterm[i], finalExam, researchPaper);
+			Assert.assertEquals(finalGrade, sciStudent.getFinalNumericGrade(), 0.1);
+			System.out.println(finalGrade);	
+		}	
+	}
+	
+	@After
+	public void cleanup () {
+		sciStudent = null;
 	}
 
+	
+	
 }
