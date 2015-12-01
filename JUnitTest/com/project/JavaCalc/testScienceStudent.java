6c71b320df38d00365a58b791fc5696d8977bc54
1amBulletproof <brandon.tarney@gmail.com>
2015-11-30 22:16:01 -0500

    Refactoring for mockito & hamcrest (unit testing)

--- JUnitTest/com/project/JavaCalc/testScienceStudent.java
+++ JUnitTest/com/project/JavaCalc/testScienceStudent.java
@@ -1,8 +1,12 @@
 
 package com.project.JavaCalc;
 
+import java.util.*;
+
 import org.junit.*;
-import org.hamcrest.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.*;
+import org.mockito.Mockito.*;
 
 
 //TODO add Mockito via Maven/Gradle build system?
@@ -13,7 +17,7 @@ public class testScienceStudent {
 
 	ScienceStudent sciStudent;
 	
-	//TODO replace method with mock framework
+
 	private double mockCalcScienceFinal(int midtermExam, int finalExam, int researchPaper){
 		double midtermWeight = 0.4;
 		double finalExamWeight = 0.4;
@@ -22,26 +26,30 @@ public class testScienceStudent {
 				 + researchPaper * researchWeight);
 	}
 	
-	//TODO implement mock framework here
+
 	@Before
 	public void initialize() {
 		sciStudent = new ScienceStudent();
 	}
 	
-	//TODO implement mock framework method from @before
+
+	/*
+	 * Tests midterm values 0-100 at intervals of 10
+	 */
 	@Test
 	public void normalTestMidterm() {
 		int[] midterm = {0,10,20,30,40,50,60,70,80,90,100};
 		int finalExam = 100;
 		int researchPaper = 100;
-		System.out.println("test");
-		for (int i = 0; i < midterm.length; i++){
-			sciStudent.calculate(midterm[i], finalExam, researchPaper);
-			System.out.println(sciStudent.getFinalNumericGrade());
-			double finalGrade = mockCalcScienceFinal(midterm[i], finalExam, researchPaper);
-			Assert.assertEquals(finalGrade, sciStudent.getFinalNumericGrade(), 0.1);
-			System.out.println(finalGrade);	
-		}	
+		ArrayList<Double> finalGrade = new ArrayList();
+		ArrayList<Double> mockFinalGrade = new ArrayList();
+		for (int mid : midterm){
+			sciStudent.calculate(mid, finalExam, researchPaper);
+			finalGrade.add( (Double) sciStudent.getFinalNumericGrade());
+			mockFinalGrade.add( (Double) mockCalcScienceFinal(mid, finalExam, researchPaper));
+		}
+//		Assert.fail();
+		assertThat(mockFinalGrade, is(not(empty())));
 	}
 	
 	@After
